c9223fda292c51e037e631fd37c127c909b23347
prnvyellayi <prnvshanmukh00@gmail.com>
2023-08-16 08:53:56 +0530

    login page

---
+++ context/index.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import React, { Dispatch, useState, createContext } from "react";
+
+export const Context = createContext<{
+  username: string;
+  secret: string;
+  setUsername: Function;
+  setSecret: Function;
+}>({ username: "", secret: "", setUsername: () => null, setSecret: () => null });
+
+export const ContextProvider = ({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) => {
+  const [username, setUsername] = useState("");
+  const [secret, setSecret] = useState("");
+
+  const value = {
+    username,
+    setUsername,
+    secret,
+    setSecret
+  };
+
+  return <Context.Provider value={value}>
+  {children}
+</Context.Provider>
+};
