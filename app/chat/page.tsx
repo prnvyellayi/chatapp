2fdf6b57bba9ddd94ccae5494e9d19126ca6a5d1
prnvyellayi <prnvshanmukh00@gmail.com>
2023-08-29 00:51:41 +0530

    proper chat UI

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -40,38 +40,73 @@ const Chat = () => {
       { username: username, message: message },
     ]);
     socket.emit("send-message", { username, message });
+    setMessage("")
   };
 
   return (
     <>
-      <div className="bg-slate-300 flex items-center justify-center h-[100vh]">
-        <div className="w-[70%] h-[80%] rounded-[20px] bg-slate-700 border-4 border-white flex flex-col justify-end">
-          {username && (
-            <div className="flex flex-col">
-              {messages.map(
-                (
-                  each: { username: string; message: string },
-                  index: number
-                ) => (
-                  <span key={index} className={`h-[fit-content] text-black bg-white max-w-[40%] rounded-[1.11vw] ${each.username === username? 'self-end rounded-tr-0' : 'self-start rounded-tl-0'}`}>
-                    {each.message}
-                  </span>
-                )
-              )}
-            </div>
-          )}
-          <form className="flex w-full px-[3vw]" onSubmit={handleSubmit}>
-            <input
-              type="text"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              className="rounded-[1.38vw] bg-white px-[1vw]"
-            ></input>
-            <button
-              type="submit"
-              className="rounded-[50%] bg-white text-black"
-            >{`->`}</button>
-          </form>
+      <div className="bg-gray-400 flex items-center justify-center h-[100vh]">
+        <div className="flex flex-row w-[70%] h-[80%] rounded-[20px] bg-white border-2 border-gray-400 overflow-hidden">
+          <div className="flex flex-col w-[30%] h-full border-r-2 border-gray-400 bg-white">
+            <span className="bg-[#eae6df] flex items-center justify-center text-[32px] h-[80px] border-b-[1px] border-gray-400">
+              CHAT MATE
+            </span>
+            <button className="w-full border-b-[1px] pl-[10px] justify-center border-gray-400 h-[60px] flex flex-col text-left">
+              <span className="text-[20px]">Room 1</span>
+              <span className="text-[14px] text-gray-400">Join Room 1</span>
+            </button>
+            <button className="w-full border-b-[1px] pl-[10px] justify-center border-gray-400 h-[60px] flex flex-col text-left">
+              <span className="text-[20px]">Room 2</span>
+              <span className="text-[14px] text-gray-400">Join Room 2</span>
+            </button>
+            <button className="w-full border-b-[1px] pl-[10px] justify-center border-gray-400 h-[60px] flex flex-col text-left">
+              <span className="text-[20px]">Room 3</span>
+              <span className="text-[14px] text-gray-400">Join Room 3</span>
+            </button>
+            <button className="w-full border-b-[1px] pl-[10px] justify-center border-gray-400 h-[60px] flex flex-col text-left">
+              <span className="text-[20px]">Room 4</span>
+              <span className="text-[14px] text-gray-400">Join Room 4</span>
+            </button>
+            <button className="w-full border-b-[1px] pl-[10px] justify-center border-gray-400 h-[60px] flex flex-col text-left">
+              <span className="text-[20px]">Room 5</span>
+              <span className="text-[14px] text-gray-400">Join Room 5</span>
+            </button>
+          </div>
+          <div className="flex flex-col justify-end w-[70%] h-full">
+            {username && (
+              <div className="flex flex-col p-[10px] overflow-scroll gap-[10px]">
+                {messages.map(
+                  (
+                    each: { username: string; message: string },
+                    index: number
+                  ) => (
+                    <span
+                      key={index}
+                      className={`max-w-[50%] rounded-[10px] p-[10px] text-[17px] break-words	${
+                        each.username === username
+                          ? "self-end rounded-tr-[0px] bg-gray-400 text-white"
+                          : "self-start rounded-tl-[0px] text-[#030303] border-2 bg-gray-100"
+                      }`}
+                    >
+                      {each.message}
+                    </span>
+                  )
+                )}
+              </div>
+            )}
+            <form className="flex w-[100%] p-[10px] justify-evenly gap-[10px] bg-[#eae6df]" onSubmit={handleSubmit}>
+              <input
+                type="text"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                className="rounded-[10px] w-[90%] bg-white px-[1vw] h-[50px] text-[17px]"
+              ></input>
+              <button
+                type="submit"
+                className="rounded-[50%] bg-white text-black w-[50px] text-[20px] h-[50px]"
+              >{`->`}</button>
+            </form>
+          </div>
         </div>
       </div>
     </>
